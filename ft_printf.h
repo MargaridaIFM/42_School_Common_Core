a94f3952740f44b809dac7f638baaccd723c9c27
mfrancis <margarida.matiass@gmail.com>
2024-03-18 20:27:05 +0000

    ft_printf Makefile e ft_prinft.h

---
+++ ft_printf.h
@@ -0,0 +1,10 @@
+#ifndef FT_PRINTF
+# define FT_PRINTF_H
+
+#include "libft.h"
+# include <stdarg.h>
+
+
+
+
+#endif
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,9 @@
+#ifndef FT_PRINTF_H
+#include FT_PRINTF_H
+
+# include <stdarg.h>
+# include "libft/libft/libft.h"
+
+int	*ft_range(int min, int max);
+
+#endif
