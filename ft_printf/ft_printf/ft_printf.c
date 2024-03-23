511b7cc345b3f1d4fe6ff82ed7f966684f5d69fa
mfrancis <margarida.matiass@gmail.com>
2024-03-23 10:37:16 +0000

    Terminado - Sem Bónus

--- ft_printf/ft_printf/ft_printf.c
+++ ft_printf/ft_printf/ft_printf.c
@@ -38,13 +38,13 @@ int	ft_conversion(va_list args, const char format)
         return (ft_putchar(va_arg(args, int)));
     else if (format == 's')
         return (ft_putstr(va_arg(args, char *)));
-    /*else if (format == 'p')
-        return (ft_putpointer( ));
-    */else if (format == 'd' || format =='i')
+    else if (format == 'p')
+        return (ft_putpointer(va_arg(args, unsigned long long)));
+    else if (format == 'd' || format =='i')
         return (ft_putnbr(va_arg(args, int)));
-    /*else if (format == 'u')
+    else if (format == 'u')
         return (ft_putunsigned(va_arg(args, unsigned int)));
-   */else if (format == 'x' || format == 'X')
+   else if (format == 'x' || format == 'X')
         return (ft_puthexadecimal(va_arg(args, unsigned int), format));
     else if (format == '%')
         return (ft_putchar('%'));
@@ -52,36 +52,55 @@ int	ft_conversion(va_list args, const char format)
     return(0);
 }
 
-/*int	main(void)
+int	main(void)
 {
+	int teste = printf("hello world\n");
+	printf("imprimiu: %d\n", teste);
+	int teste2 = ft_printf("hello world\n");
+	ft_printf("imprimiu: %d\n", teste2);
+	printf("\n");
+	int x = 42;
+	int *ptr = &x; 
 	ft_printf("HELLO\n");
     ft_printf("CHARACTERS: %c %c\n", 'a', 'b');
     ft_printf("STRINGS: %s %s\n", "Good", "bye");
-    ft_printf("DECIMAL: %d %i\n", 42, 123.45);
+    ft_printf("DECIMAL: %i %d\n", 42, 123);
 	ft_printf ("DECIMALS: %d %d\n", 1977, 650000L);
-	ft_printf ("PRECEDING WITH BLANKS: %10d \n", 1977);
-	ft_printf ("PRECEDING WITH ZEROS: %010d \n", 1977);
-	ft_printf ("SOME DIFFERENTS RADICES: %d %x %o \n", 255, 255, 255);
-	ft_printf ("SOME DIFFERENT RADICES: %#X %#o \n", 255, 255);
-	ft_printf ("FLOATS: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
-	ft_printf ("WITH TRICK: %*d \n", 5, 10);
-    ft_printf("\n");
-    
+	ft_printf("POINTER NULO: %p \n", NULL );
+	ft_printf("POINTER DE x é: %p\n", (void *)ptr);
+	ft_printf ("DECIMAL e HEXADECIMAL: %d %x %X \n", 255, 255, 255);
+	ft_printf ("Unsigned int: %u \n", UINT_MAX);
+	ft_printf ("Unsigned int: %u \n", 4294967296);
+	
+   	ft_printf("\n");
+	
 	printf("Hello\n");
 	printf("Characters: %c %c\n", 'a', 'b');
 	printf("Strings: %s %s\n", "Good", "bye");
-	printf("Decimal: %d %f\n", 42, 123.45);
+	printf("Decimal: %d %d\n", 42, 123);
 	printf ("Decimals: %d %ld\n", 1977, 650000L);
-	printf ("Preceding with blanks: %10d \n", 1977);
+	printf("Pointer nulo: %p \n", NULL );
+	printf("Pointer de x é: %p\n", (void *)ptr);
+	printf ("Decimal e hexadecimal: %d %x %X \n", 255, 255, 255);
+	printf ("Unsigned int: %u \n", UINT_MAX);
+	ft_printf ("Unsigned int: %u \n", 4294967296 );
+	return (0);
+}
+/*
+printf ("Preceding with blanks: %10d \n", 1977);
 	printf ("Preceding with zeros: %010d \n", 1977);
 	printf ("Some different radices: %d %x %o \n", 255, 255, 255);
-	printf ("Some different radices: %#X %#o \n", 255, 255);
+	printf ("Some different radices: %#x %#o \n", 255, 255);
 	printf ("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
-	printf ("Width trick: %*d \n", 5, 10);  
+	printf ("Width trick: %*d \n", 5, 10); 
 
-	return (0);
-}*/
-
+	ft_printf ("PRECEDING WITH BLANKS: %10d \n", 1977);
+	ft_printf ("PRECEDING WITH ZEROS: %010d \n", 1977);
+	ft_printf ("SOME DIFFERENTS RADICES: %d %x %o \n", 255, 255, 255);
+	ft_printf ("SOME DIFFERENT RADICES: %#x %#o \n", 255, 255);
+	ft_printf ("FLOATS: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+	ft_printf ("WITH TRICK: %*d \n", 5, 10);
+    */ 
 /*Pseudocode:
 formato do printf original:
 printf("Caracter: %--", variavel)
